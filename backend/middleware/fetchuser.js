f43de60ff12ab0e2ed5b043de51d567f4d3f9274
prasadvkamat <prasadvkamat2001@gmail.com>
2023-08-21 00:25:24 +0530

    night edit

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,16 @@
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = "prasadkamat9844"
+const fetchuser = (req, res, next) => {
+    const token = req.header('auth_token');
+    if (!token) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+}
+module.exports = fetchuser;
